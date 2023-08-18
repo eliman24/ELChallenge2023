8f9f62c5435e0152b3c4b819bc853563f2df664c
Elisabeth Manalo <eliman24@bergen.org>
2023-08-18 11:37:37 -0400

    Initial Commit

---
+++ ELChallenge2023/ELChallenge2023App.swift
@@ -0,0 +1,17 @@
+//
+//  ELChallenge2023App.swift
+//  ELChallenge2023
+//
+//  Created by Elisabeth Manalo on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ELChallenge2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
